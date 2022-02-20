1c0e658393331b7472f0e08a5993e8a14186ea24
Yulia Koshkina <julyasav4uk@gmail.com>
2022-02-20 23:01:49 +0200

    fixed

--- src/pages/Button/Button.jsx
+++ src/pages/Button/Button.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './button.scss';
 
-export const Button = () => {
-  return (
-    <div className="page__section">
-      <div className="container bb">
-        <button className="button">
-          Get Started
-        </button>
-      </div>
+export const Button = () => (
+  <div className="page__section">
+    <div className="container bb">
+      <button type="button" className="button">
+        Get Started
+      </button>
     </div>
-  )
-}
+  </div>
+);
--- src/pages/Catalog/Books/Books.jsx
+++ src/pages/Catalog/Books/Books.jsx
@@ -1,123 +1,121 @@
 import React from 'react';
 import './books.scss';
 
-export const Books = () => {
-  return (
-    <div className="books">
-      <div className="books__container">
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-          <img
-            className="books_image"
-            src="images/books/1.png"
-            alt="book"
-          />
-        </div>
-        <div className="books__book">
-        <img
-          className="books_image"
-          src="images/books/1.png"
-          alt="book"
-        />
-        </div>
-        <div className="books__book">
+export const Books = () => (
+  <div className="books">
+    <div className="books__container">
+      <div className="books__book">
         <img
           className="books_image"
           src="images/books/1.png"
           alt="book"
         />
       </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
+      </div>
+      <div className="books__book">
+        <img
+          className="books_image"
+          src="images/books/1.png"
+          alt="book"
+        />
       </div>
     </div>
-  )
-}
+  </div>
+);
